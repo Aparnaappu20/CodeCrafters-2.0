94344ecb53a1e78b7d8c148f4d9cd1e76657024e
Aparnaappu20 <aparnamanoj017@gmail.com>
2024-10-09 10:46:22 +0000

    python day2

---
+++ main.py
@@ -0,0 +1,28 @@
+print("Hello World")
+print("APARNA MANOJ")
+num1: int=10
+print(num1)
+print("this is an interger", num1)
+name: str = input("Enter your name")
+print(name)
+x=10
+y=87
+sum=x+y 
+print(sum)
+word="hii how are you"
+sub1=word[0:5]
+print(sub1)
+sub2=word[:7]
+print(sub2)
+print("THE NAME IS", name)
+num1: int = int(input("enter a num 1: "))
+num2: int = int(input("enter a num 2: "))
+
+if num1 == num2:
+   print("both are same")
+elif num1>num2:
+    print(num1,"is greater")
+elif num1<num2:
+    print(num2,"is greater")
+else:
+   print("both are different")
